ef504adaf9d7afafc117ceefa806dc28582a7949
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-04 16:51:15 +0900

    도전 문제 3 해결

--- C_example/1_3.c
+++ C_example/1_3.c
@@ -1,51 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int BigCommonNum(int, int);
+int BigCommonNum();
 
-int main(void)
+int main()
 {
-	int num1, num2;
-
-	printf("정수 2개 입력: ");
-	scanf("%d %d", &num1, &num2);
-
-	BigCommonNum(num1, num2);
+	BigCommonNum();
 
 	return 0;
 }
 
-int BigCommonNum(int n1, int n2)
+int BigCommonNum()
 {
-	int sn, bn;
+	int n1, n2;
 
-	if (n1 < n2)
-	{
-		sn = n1;
-		bn = n2;
-	}
-	else if (n1 > n2)
-	{
-		sn = n2;
-		bn = n1;
-	}
-	else if (n1 == n2)
-		printf("%d와 %d의 최대공약수는 %d이다.\n", n1, n2, n1);
-	else if (n1 == 1 || n2 == 1)
-		printf("%d와 %d의 최대공약수는 %d이다.\n", n1, n2, n1);
+	printf("두 정수 입력: ");
+	scanf("%d %d", &n1, &n2);
 
-	if (bn % sn == 0)
-		printf("%d와 %d의 최대공약수는 %d이다.\n", bn, sn, sn);
-	else
+	for (int i = n1; i <= n1; i--)
 	{
-		for (int i = sn - 1; i < sn; i--)
+		if (n1 % i == 0 && n2 % i == 0)
 		{
-			if (sn % i == 0 && bn % i == 0)
-			{
-				printf("%d와 %d의 최대공약수는 %d이다.\n", bn, sn, i);
-
-				break;
-			}
+			printf("%d와 %d의 최대공약수는 %d이다.\n", n1, n2, i);
+			
+			break;
 		}
 	}
 }
