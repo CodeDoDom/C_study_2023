f689e0cc8bed0c8ac7bf808b2f8350d334ab8333
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-14 16:59:59 +0900

    문제 11_1_1 해결

--- C_example/11_1_1.c
+++ C_example/11_1_1.c
@@ -4,22 +4,33 @@
 int main()
 {
 	int arr[5];
-	int tot = 0;
+	int tot = 0, min = 0, max = 0, temp = 0;
 
 	printf("정수 5개 입력: ");
 
 	for (int i = 0; i < 5; i++)
 		scanf("%d", &arr[i]);
 
-	for (int i = 0; i < 5; i++)
-		printf("%d\n", arr[i]);
+	for (int i = 0; i < 5 - 1; i++)
+	{
+		for (int j = 0; j < 5 - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	printf("입력된 정수 중에서 최댓값은 %d이다.\n", arr[4]);
+	printf("입력된 정수 중에서 최솟값은 %d이다.\n", arr[0]);
 
 	for (int i = 0; i < 5; i++)
 	{
 		tot += arr[i];
 	}
-
-	printf("배열의 총 합은 %d이다.\n", tot);
+	printf("입력된 정수의 총 합은 %d이다.\n", tot);
 
 	return 0;
 }
