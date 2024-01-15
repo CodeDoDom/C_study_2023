f1fb20fa86dedebd857c9b485527ef40411deced
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-15 16:43:03 +0900

    문제 12_1_2 해결

--- C_example/12_1_2.c
+++ C_example/12_1_2.c
@@ -15,7 +15,8 @@ int main(void)
 int MySolution()
 {
 	int num1 = 10, num2 = 20;
-	int* ptr1 = &num1, * ptr2 = &num2;
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
 
 	*ptr1 += 10;
 	*ptr2 -= 10;
@@ -23,11 +24,21 @@ int MySolution()
 	ptr1 = &num2;
 	ptr2 = &num1;
 
-	printf("%d %d", *ptr1, *ptr2);
+	printf("%d %d\n", *ptr1, *ptr2);
 }
 
 int BookSolution()
 {
 	int num1 = 10, num2 = 20;
-	int* ptr1 = &num1, *
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	int* temp;
+	(*ptr1) += 10;
+	(*ptr2) -= 10;
+
+	temp = ptr1;
+	ptr1 = ptr2;
+	ptr2 = temp;
+
+	printf("%d %d\n", *ptr1, *ptr2);
 }
