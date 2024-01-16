b57d5847d4d8777c66ff02e78f425f5da900d9a0
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-16 12:30:23 +0900

    문제 14_2_2 정답 주석 추가

--- C_example/14_2_2.c
+++ C_example/14_2_2.c
@@ -5,6 +5,10 @@ void ShowData(const int* ptr)
 	int* rptr = ptr;
 	printf("%d \n", *rptr);
 	*rptr = 20;
+
+	// 지적할만한 사항
+	// : 함수의 매개변수 ptr을 const로 선언하여 안에 저장된 값을 수정할 수 없게끔 하였는데,
+	// 그걸 굳이 *rptr에 집어넣어서 값을 수정함. 굳이? 왜?
 }
 
 int main(void)
