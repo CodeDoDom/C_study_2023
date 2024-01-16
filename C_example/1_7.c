c9adc3afdd80c47da9990b585419482e6f8397e3
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-16 13:10:26 +0900

    도전 문제 7 최종 해결

--- C_example/1_7.c
+++ C_example/1_7.c
@@ -1,27 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int Number1(int n)
+int SquareNum(int n)
 {
 	int res = 1;
+	int cnt = 0;
 
-	for (int i = 0; i < n; i++)
+
+	while (1)
 	{
-		res = res * 2;
+		res *= 2;
+		if (res > n)
+			break;
+		cnt++;
 	}
 
-	printf("2의 %d제곱은 %d\n", n, res);
-}
-
-int Number2(int n)
-{
-	int m;
-
-	// 2의 k제곱은 n
-	/*while (n % 2 < 2)
-	{
-		m = n % 2;
-	}*/
+	printf("2^%d <= %d\n", cnt, n);
 }
 
 int main()
@@ -31,7 +25,7 @@ int main()
 	printf("정수 입력: ");
 	scanf("%d", &n);
 
-	Number(n);
+	SquareNum(n);
 
 	return 0;
 }
