d7c248d69b098238a743d7c4857e668cacb21b26
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-16 15:24:40 +0900

    도전2 문제 2 해결

---
+++ C_example/2_2.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void TEMPBinaryNum(int num)
+{
+	if (num % 2 == 0)
+		printf("0 ");
+	else
+		printf("1 ");
+
+	if (num > 0)
+		TEMPBinaryNum(num / 2);
+}
+
+void BinaryNum(int num)
+{
+	int arr[50] = { 0 };
+	int len = 0;
+
+	while (num > 0)
+	{
+		arr[len] = num % 2;
+		num /= 2;
+		len++;
+	}
+
+	for (int i = len - 1; i >= 0; i--)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+int main(void)
+{
+	int num;
+
+	printf("10진수 정수 입력: ");
+	scanf("%d", &num);
+
+	TEMPBinaryNum(num);
+	printf("\n");
+	BinaryNum(num);
+
+	return 0;
+}
