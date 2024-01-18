9c311e791495948965131746f90965ed5e31fed5
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-18 17:52:21 +0900

    문제 17_1_1 해결

---
+++ C_example/17_1_1.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void MaxAndMin(int arr[], int len, int** maxPtr, int** minPtr);
+
+int main(void)
+{
+	int* maxPtr;
+	int* minPtr;
+	int arr[5];
+
+	printf("숫자 5개 입력\n");
+	for (int i = 0; i < 5; i++)
+	{
+		printf("입력: ");
+		scanf("%d", &arr[i]);
+	}
+
+	MaxAndMin(arr, sizeof(arr) / sizeof(int), &maxPtr, &minPtr);
+
+	printf("가장 큰 값: %d\n가장 작은 값: %d\n", *maxPtr, *minPtr);
+
+	return 0;
+}
+
+void MaxAndMin(int* arr, int len, int** maxPtr, int** minPtr)
+{
+	int* max, * min;
+
+	max = &arr[0];
+	min = &arr[0];
+
+	for (int i = 0; i < len; i++)
+	{
+		if (*max < arr[i])
+			max = &arr[i];
+
+		if (*min > arr[i])
+			min = &arr[i];
+	}
+
+	*maxPtr = max;
+	*minPtr = min;
+}
