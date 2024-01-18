538b15f9ef9f0bebd8c6c2d22e4280561d7ea74c
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-18 19:14:00 +0900

    문제 18_1_2 해결

---
+++ C_example/18_1_2.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void SimpleFuncOne( int *arr1, int *arr2 )
+{
+	printf("§\n");
+}
+
+void SimpleFuncTwo( int (*arr3)[4], int (*arr4)[4])
+{
+	printf("§§\n");
+}
+
+int main(void)
+{
+	int arr1[3];
+	int arr2[4];
+	int arr3[3][4];
+	int arr4[2][4];
+
+	SimpleFuncOne(arr1, arr2);
+	SimpleFuncTwo(arr3, arr4);
+
+	return 0;
+}
