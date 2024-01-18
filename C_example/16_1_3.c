26b1554d0e8b9b4dd807fe688db44ec1451c51b2
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-18 16:34:10 +0900

    문제 16_1_3 해결

--- C_example/16_1_3.c
+++ C_example/16_1_3.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	int student_info[5][5];
+	int student_info[5][5] = { 0 };
 
 	for (int i = 0; i < 4; i++)
 	{
@@ -11,7 +11,7 @@ int main(void)
 
 		for (int j = 0; j < 4; j++)
 		{
-			switch (j)
+			switch (j + 1)
 			{
 			case 1:
 				printf("국어: ");
@@ -29,9 +29,41 @@ int main(void)
 				break;
 
 			case 4:
+				printf("국사: ");
+				scanf("%d", &student_info[i][j]);
+				break;
+
+			default:
+				break;
 			}
 		}
 	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			student_info[i][4] += student_info[i][j];
+		}
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			student_info[4][i] += student_info[i][j];
+		}
+		student_info[4][4] += student_info[4][i];
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+			printf("%5d ", student_info[i][j]);
+
+		printf("\n");
+	}
+
 
 	return 0;
 }
