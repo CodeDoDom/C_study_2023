ee7f88e0ce1d779c359d94078636aa13d54e5864
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-01-22 18:37:37 +0900

    문제 18_1_3,4,5 해결

--- C_example/18_1_3.c
+++ C_example/18_1_3.c
@@ -1,9 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void ComplexFuncOne(int** arr1, int* (*arr2)[5])
+{
+	printf("1\n");
+}
+
+void ComplexFuncTwo(int*** arr3, int*** (*arr4)[5])
+{
+	printf("2\n");
+}
+
 int main(void)
 {
+	int* arr1[3];
+	int* arr2[3][5];
+	int** arr3[5];
+	int*** arr4[3][5];
 
+	ComplexFuncOne(arr1, arr2);
+	ComplexFuncTwo(arr3, arr4);
 
 	return 0;
 }
