a0b046cbf7bc35cb7c2f15b7f52eda0215b879ce
2022182024(laptop) <lhnyoon@tukorea.ac.kr>
2024-02-09 06:51:33 +0900

    도전4 문제 1 수정

--- C_example/4_1.c
+++ C_example/4_1.c
@@ -1,64 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+#define LEN 50
+
 typedef struct book
 {
-	char writer[50];
-	char title[50];
+	char writer[LEN];
+	char title[LEN];
 	int page;
 } Book;
 
 int main(void)
 {
-	Book book1, book2, book3;
+	Book book[3];
 
 	for (int i = 0; i < 3; i++)
 	{
-		if (i == 0)
-		{
-			printf("历磊: ");
-			fgets(book1.writer, sizeof(book1.writer), stdin);
-			printf("力格: ");
-			fgets(book1.title, sizeof(book1.title), stdin);
-			printf("其捞瘤: ");
-			scanf("%d", &book1.page);
-			getchar();
-		}
-
-		else if (i == 1)
-		{
-			printf("历磊: ");
-			fgets(book2.writer, sizeof(book2.writer), stdin);
-			printf("力格: ");
-			fgets(book2.title, sizeof(book2.title), stdin);
-			printf("其捞瘤: ");
-			scanf("%d", &book2.page);
-			getchar();
-		}
-
-		else
-		{
-			printf("历磊: ");
-			fgets(book3.writer, sizeof(book3.writer), stdin);
-			printf("力格: ");
-			fgets(book3.title, sizeof(book3.title), stdin);
-			printf("其捞瘤: ");
-			scanf("%d", &book3.page);
-			getchar();
-		}
+		printf("历磊: ");
+		fgets(book[i].writer, LEN, stdin);
+		
+		printf("力格: ");
+		fgets(book[i].title, LEN, stdin);
+		
+		printf("其捞瘤: ");
+		scanf("%d", &book[i].page);
+		getchar();
 	}
 
 	for (int i = 0; i < 3; i++)
-	{
-		if (i == 0)
-			printf("book1\n历磊: %s\n力格: %s\n其捞瘤: %d\n\n", book1.writer, book1.title, book1.page);
-
-		else if (i == 1)
-			printf("book2\n历磊: %s\n力格: %s\n其捞瘤: %d\n\n", book2.writer, book2.title, book2.page);
-
-		else
-			printf("book3\n历磊: %s\n力格: %s\n其捞瘤: %d\n\n", book3.writer, book3.title, book3.page);
-	}
+		printf("[ book%d ]\n历磊: %s\n力格: %s\n其捞瘤: %d\n\n", i + 1, book[i].writer, book[i].title, book[i].page);
 
 	return 0;
 }
